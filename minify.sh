7eea6a18909bd9ca7dcb2612de8e061d50e4b67f
Michael Grosser <michael@grosser.it>
2017-01-05 08:09:16 -0800

    minify last PR

--- minify.sh
+++ minify.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
-#needs reduce -- http://github.com/grosser/reduce
+#!/bin/sh
+which reduce 2>&1 || echo "needs reduce: gem install reduce ... see http://github.com/grosser/reduce"
 reduce s3-slider.js > s3-slider.min.js
 reduce s3-slider.css > s3-slider.min.css
